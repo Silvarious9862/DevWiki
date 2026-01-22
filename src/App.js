e2daf3b024e8c6e4e3779a7a0c5c91918d76303a
Silvarious9862 <vbobrik0@gmail.com>
2026-01-22 09:19:23 +0300

    layout added to app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SystemHealthWidget from "./SystemHealthWidget";
 import NotFound404 from "./NotFound404";
+import MainLayout from "./layout/MainLayout";
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* корневая страница */}
-        <Route path="/" element={<h1>Wiki Frontend</h1>} />
+        <Route 
+          path="/" 
+          element={
+            <MainLayout>
+              <h1>Wiki Frontend</h1>
+            </MainLayout>
+          } 
+        />
 
         {/* health-страница */}
         <Route path="/health" element={<SystemHealthWidget />} />
